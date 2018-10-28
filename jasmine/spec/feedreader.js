4781587c7c91b5c1faaf1f2f23df94e2bfa22bab
aerasmus <addo.erasmmus@gmail.com>
2018-10-28 15:06:26 +0000

    reviewed feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -21,27 +21,27 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
+        
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
 
-         /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
         it('urls are defined', function() {
             for(let i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].url).toBeDefined();
                 expect(allFeeds[i].url.length).not.toBe(0);
             }
         });
+
+        /* A test that loops through each feed
+         * in the allFeeds object and ensures it has a name defined
+         * and that the name is not empty. */
 
         it('names are defined', function () {
             for(let i = 0; i < allFeeds.length; i++) {
@@ -51,26 +51,22 @@ $(function() {
         });  
     });
 
-
-    /* TODO: Write a new test suite named "The menu" */
-        
-
-        /* TODO: Write a test that ensures the menu element is
+        /* A test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
 
-         /* TODO: Write a test that ensures the menu changes
+    describe('The Menu', function () {
+        it('menu element is hidden', function () {
+                expect($('body').hasClass('menu-hidden')).toEqual(true);
+            });
+
+        /* A test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-        describe('The Menu', function () {
-            it('menu element is hidden', function () {
-                expect($('body').hasClass('menu-hidden')).toEqual(true);
-            });
-
             it('working toggle on click event', function () {
                 $('.menu-icon-link').trigger('click');
                 expect($('body').hasClass('menu-hidden')).toBe(false);
@@ -79,15 +75,13 @@ $(function() {
             });
         });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
+     
+        /* A test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
     describe('Initial Entries', function () {
         beforeEach(function (done) {
             loadFeed(0, function () {
@@ -96,17 +90,14 @@ $(function() {
         });
 
         it('define if entry has more 0 entries', function () {
-            expect($('.entry .feed')).toBeDefined();
+            expect($('.feed .entry').lenght).toBeGreaterThan(0);
         });
     });    
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* A test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-
     describe('New Feed Selection', function () {
         let oldFeed;
         beforeEach(function(done) {
